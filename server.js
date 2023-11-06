71e3e462e1e89207aa96edbf373e6b8d89769861
Arafat Hossain <arafat15-12-603@diu.edu.bd>
2023-11-06 18:18:00 +0600

    MongoDB with mongoosh

--- server.js
+++ server.js
@@ -1,8 +1,56 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
-console.log(process.env);
+
+// console.log(dotenv('env'));
 
 const app = require('./app.js');
+
+const DataBase = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DataBase, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DataBase Connection Successfully!!!'));
+
+const tourSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  rating: 4.7,
+  price: 997,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('Error :', err);
+  });
 //   START SERVER
 const port = process.env.PORT || 2000;
 app.listen(port, () => {

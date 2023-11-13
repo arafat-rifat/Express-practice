020126c1ec4492b02f1ebc5ada51dee51e1ba5a8
Arafat Hossain <arafat15-12-603@diu.edu.bd>
2023-11-13 14:53:03 +0600

    Aggregation Pipeline

--- Controller/tourController.js
+++ Controller/tourController.js
@@ -106,3 +106,32 @@ exports.deleteTour = async (req, res) => {
     });
   }
 };
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: null,
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      status: 'Sucess',
+      data: stats,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'Fail',
+      message: err,
+    });
+  }
+};
